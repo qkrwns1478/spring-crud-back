7c0332e7bde5ac2bd014eb93168523cc41288921
qkrwns1478 <qkrwns1478@gmail.com>
2026-03-09 19:45:50 +0900

    first commit

---
+++ src/main/java/com/example/springcrudback/SpringCrudBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcrudback/SpringCrudBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcrudback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
